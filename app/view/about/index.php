0435fa373e1de90d3baaf69a82210cf8f079a521
farhanpraw <farhanprawira69@yahoo.com>
2019-10-08 01:40:59 +0700

    delete

--- app/view/about/index.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-
-
-		<h1 class="mt-5">About Me</h1>
-		<img src="<?= BASEURL; ?>/img/FarhanPraw.JPG" alt="FarhanPraw" width="300" class="rounded-circle shadow" >
-		<p>Halo, Nama Saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya selalu <?= $data['pekerjaan']; ?> </p>
-</div>
--- app/view/home/home.php
+++
@@ -1,1 +0,0 @@
-<h1>Selamat Datang di Website Farhan Prawiradinata</h1>
